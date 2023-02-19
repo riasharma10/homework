629351312d84a2f1122cf93d88a9db1151efd44f
Ria Sharma <riassharma10@gmail.com>
2023-02-19 13:38:34 -0500

    RegOPT

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -6,25 +6,27 @@ from torchvision.transforms import Compose, Normalize, ToTensor
 
 
 class CONFIG:
-
     batch_size = 64
-    num_epochs = 2
-    initial_learning_rate = 0.001
-    initial_weight_decay = 0
+    num_epochs = 15
+    initial_learning_rate = 0.005
+    initial_weight_decay = 0.001
 
     lrs_kwargs = {
         # You can pass arguments to the learning rate scheduler
-        # constructor here.
+        # # constructor here.
     }
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.Adam(
-        model.parameters(), lr=CONFIG.initial_learning_rate, weight_decay=CONFIG.initial_weight_decay,
+        model.parameters(),
+        lr=CONFIG.initial_learning_rate,
+        weight_decay=CONFIG.initial_weight_decay,
     )
 
     transforms = Compose(
         [
+            # Normalize((0.1307,), (0.3081,)),
             ToTensor(),
         ]
     )
--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -4,21 +4,56 @@ from torch.optim.lr_scheduler import _LRScheduler
 
 
 class CustomLRScheduler(_LRScheduler):
-    def __init__(self, optimizer, last_epoch=-1):
+    """
+    Create a new scheduler.
+
+    """
+
+    def __init__(self, optimizer, model_size=512, factor=1, warmup=4000):
         """
-        Create a new scheduler.
+        Initialize a new scheduler.
 
         Note to students: You can change the arguments to this constructor,
         if you need to add new parameters.
 
         """
-        # ... Your Code Here ...
-        super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
+        self.optimizer = optimizer
+        self._step = 0
+        self.warmup = warmup
+        self.model_size = model_size
+        self.factor = factor
+        self._rate = 0
+        super(CustomLRScheduler, self).__init__(optimizer, -1)
+
+    def step(self):
+        "Update parameters and rate"
+        self._step += 1
+        rate = self.rate()
+        for p in self.optimizer.param_groups:
+            p["lr"] = rate
+        self._rate = rate
+        self.optimizer.step()
+
+    def rate(self, step=None):
+        "Implement `lrate` above"
+        if step is None:
+            step = self._step
+        return self.model_size ** (-0.5) * min(
+            step ** (-0.5), step * self.warmup ** (-1.5)
+        )
 
     def get_lr(self) -> List[float]:
+        """
+        Create a new get_lr method for scheduler.
+
+        """
         # Note to students: You CANNOT change the arguments or return type of
         # this function (because it is called internally by Torch)
 
         # ... Your Code Here ...
-        # Here's our dumb baseline implementation:
-        return [i for i in self.base_lrs]
+        # if self.last_epoch % self.decay_steps == 0 and self.last_epoch != 0:
+        #     for param_group in self.optimizer.param_groups:
+        #         param_group['lr'] *= self.decay_rate
+        #         # print("param_group['lr'] = ", param_group['lr'])
+
+        return [param_group["lr"] for param_group in self.optimizer.param_groups]
