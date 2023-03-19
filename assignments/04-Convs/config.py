6849aa27d42953bb888bdbd180fc0ae695c7d1f8
Ria Sharma <riassharma10@gmail.com>
2023-03-19 05:41:21 -0400

    works

--- assignments/04-Convs/config.py
+++ assignments/04-Convs/config.py
@@ -2,20 +2,19 @@ from typing import Callable
 import torch
 import torch.optim
 import torch.nn as nn
-from torchvision.transforms import Compose, ToTensor, RandomHorizontalFlip
+from torchvision.transforms import Compose, ToTensor
 
 
 class CONFIG:
-    batch_size = 32
-    num_epochs = 3
+    batch_size = 16
+    num_epochs = 5
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
-    ] = lambda model: torch.optim.AdamW(model.parameters(), lr=1e-3)
+    ] = lambda model: torch.optim.Adam(model.parameters(), lr=1e-3)
 
     transforms = Compose(
         [
-            RandomHorizontalFlip(),
             ToTensor(),
         ]
     )
--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -9,7 +9,7 @@ class Model(torch.nn.Module):
 
     def __init__(self, num_channels: int, num_classes: int) -> None:
         """
-        Initialize the model.
+        Init.
         """
         super(Model, self).__init__()
 
@@ -17,26 +17,19 @@ class Model(torch.nn.Module):
         self.norm1 = nn.BatchNorm2d(32)
         self.relu1 = nn.ReLU(inplace=True)
 
-        self.conv2 = nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1)
-        self.norm2 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 16, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(16)
         self.relu2 = nn.ReLU(inplace=True)
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
 
-        self.fc1 = nn.Linear(32 * 16 * 16, 64)
-        self.relu4 = nn.ReLU(inplace=True)
-        self.dropout1 = nn.Dropout(0.5)
-
-        self.fc2 = nn.Linear(64, num_classes)
-
-        self.fc3 = nn.Linear(32 * 16 * 16, num_classes)
+        self.fc3 = nn.Linear(16 * 16 * 16, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
         Forward pass
         """
         x = self.relu1(self.norm1(self.conv1(x)))
-        x = self.pool1(self.relu2(self.norm2(self.conv2(x))))
-
+        x = self.pool1(self.relu2(self.bn2(self.conv2(x))))
         x = x.view(x.size(0), -1)
         x = self.fc3(x)
 
